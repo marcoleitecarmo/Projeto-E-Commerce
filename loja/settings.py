5ebcd0203c2ee8e4079e290873da69d8252dd95f
marcoleitecarmo <marcoleitecarmo@gmail.com>
2022-09-29 16:35:43 -0300

    correções

--- loja/settings.py
+++ loja/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 from pathlib import Path
-from django.contrib.messages import constants
-from decouple import config
 
+from decouple import config
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
