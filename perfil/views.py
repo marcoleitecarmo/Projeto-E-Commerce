5a9b3d8bef7abdd7c830d24c1709e34e03eeecfe
Marco Antônio Leite do Carmo <marcoleitecarmo@gmail.com>
2022-10-25 11:07:32 -0300

    novas atualizações

--- perfil/views.py
+++ perfil/views.py
@@ -1,5 +1,4 @@
 import copy
-
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
@@ -7,7 +6,6 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
 from django.views.generic import ListView
-
 from . import forms, models
 
 
